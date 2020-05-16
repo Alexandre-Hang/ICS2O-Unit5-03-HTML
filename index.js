951b01adc825a1eb415bb29947a1252c41aa59f9
Ubuntu <ubuntu@ip-172-31-93-59.ec2.internal>
2020-05-16 19:49:20 +0000

    Saved all

---
+++ index.js
@@ -0,0 +1,17 @@
+// JavaScript File
+var age;
+function movieWatch() {
+  age = document.getElementById("age").value;
+  if (age >= 17)
+  {
+    alert ("You can watch any R rated or less rated movie alone");
+  }
+  else if (age >= 13)
+  {
+    alert("You can watch any PG-13 or less rated movie alone");
+  }
+  else
+  {
+    alert("You can watch amy G or PG rated movie alone");
+  }
+}
